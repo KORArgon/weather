6b0b972e359fa1736dcc4cbb459782acd7da40c7
LEE-HYO-SANG\hanshin <uon0707@naver.com>
2024-02-05 17:05:07 +0900

    페이징 처리 추가 및 주석 추가

--- src/main/java/com/argon/weather/controller/WeatherController.java
+++ src/main/java/com/argon/weather/controller/WeatherController.java
@@ -3,17 +3,16 @@ package com.argon.weather.controller;
 import com.argon.weather.domain.Weather;
 import com.argon.weather.service.MessageService;
 import com.argon.weather.service.WeatherService;
+import com.argon.weather.util.PagingUtil;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.parser.ParseException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,52 +30,92 @@ public class WeatherController {
      * @return
      */
     @GetMapping("/weather/weatherListForm")
-    public String weatherListForm(Model model, Pageable pageable){
-        List<Weather> weatherList = weatherService.findAll(pageable);
+    public String weatherListForm(Model model, @PageableDefault(page=0, size=10, sort="weatherId", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<Weather> weatherList = weatherService.findAll(pageable);
         model.addAttribute("weatherList", weatherList);
-        model.addAttribute("pageable", pageable);
+        PagingUtil.getPaginationInfo(model, weatherList);
+
         return "weather/weatherList";
     }
 
+    /**
+     * 날씨 상세 조회
+     * @param weather
+     * @param model
+     * @return
+     */
     @GetMapping("/weather/weatherViewForm")
     public String weatherViewForm(Weather weather, Model model){
-        Optional<Weather> result = weatherService.findById(weather.getWeatherId());
-        model.addAttribute("result", result);
+        Weather result = weatherService.findById(weather.getWeatherId());
+        model.addAttribute("weather", result);
         return "weather/weatherView";
     }
 
+    /**
+     * 날씨 등록 페이지
+     * @return
+     */
     @GetMapping("/weather/weatherRegistForm")
     public String weatherRegistForm(){
         return "weather/weatherRegist";
     }
 
+    /**
+     * 날씨 등록 처리
+     * @param weather
+     * @param model
+     * @return
+     */
     @PostMapping("/weather/weatherRegist")
-    public String weatherRegist(Weather weather){
+    public String weatherRegist(Weather weather, Model model){
         weatherService.save(weather);
-        return "weather/weatherRegist";
+        return messageService.redirectMessage(model, "등록을 완료했습니다.", "/weather/weatherListForm");
     }
 
+    /**
+     * 날씨 수정 페이지
+     * @param weather
+     * @param model
+     * @return
+     */
     @GetMapping("/weather/weatherUpdateForm")
     public String weatherUpdateForm(Weather weather, Model model){
-        Optional<Weather> result = weatherService.findById(weather.getWeatherId());
-        model.addAttribute("result", result);
+        Weather result = weatherService.findById(weather.getWeatherId());
+        model.addAttribute("weather", result);
         return "weather/weatherUpdate";
     }
 
+    /**
+     * 날씨 수정 처리
+     * @param weather
+     * @param model
+     * @return
+     */
     @PostMapping("/weather/weatherUpdate")
-    public String weatherUpdate(Weather weather){
-
-        return "weather/weatherUpdate";
+    public String weatherUpdate(Weather weather, Model model){
+        weatherService.save(weather);
+        return messageService.redirectMessage(model, "수정을 완료했습니다.", "/weather/weatherListForm");
     }
 
+    /**
+     * 날씨 삭제 처리
+     * @param weather
+     * @param model
+     * @return
+     */
     @GetMapping("/weather/weatherDelete")
-    public String weatherDelete(Model model, Weather weather){
+    public String weatherDelete(Weather weather, Model model){
         weatherService.weatherDelete(weather);
         return messageService.redirectMessage(model, "삭제를 완료했습니다.", "/weather/weatherListForm");
     }
 
+    /**
+     * 날씨 Api 등록 처리
+     * @param model
+     * @return
+     */
     @GetMapping("/weather/weatherRegistAuto")
-    public String weatherRegistAuto(Model model) throws IOException, ParseException {
+    public String weatherRegistAuto(Model model) {
         weatherService.saveAuto();
         return messageService.redirectMessage(model, "등록을 완료했습니다.", "/weather/weatherListForm");
     }
--- src/main/java/com/argon/weather/service/WeatherService.java
+++ src/main/java/com/argon/weather/service/WeatherService.java
@@ -3,6 +3,7 @@ package com.argon.weather.service;
 import com.argon.weather.domain.Weather;
 import com.argon.weather.repository.WeatherRepository;
 import com.argon.weather.util.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -13,37 +14,75 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.List;
-import java.util.Optional;
+
+import org.springframework.data.domain.Page;
 
 @Service
+@Slf4j
 public class WeatherService {
 
-    @Value("${weather.api.key}")
-    private String apiKey;
-
+    // weatherService
     private final WeatherRepository weatherRepository;
 
+    // weatherService
     public WeatherService(WeatherRepository weatherRepository) {
         this.weatherRepository = weatherRepository;
     }
+    
+    // API KEY 값, application-local.properties 에서 설정
+    @Value("${weather.api.key}")
+    private String API_KEY;
 
+    /**
+     * 전체 조회
+     * @param pageable
+     * @return
+     */
+    public Page<Weather> findAll(Pageable pageable) {
+        return weatherRepository.findAll(pageable);
+    }
+
+    /**
+     * 상세 조회
+     * @param id
+     * @return
+     */
+    public Weather findById(Long id){
+        return weatherRepository.findById(id).orElseThrow();
+    }
+
+    /**
+     * 등록 및 수정 처리
+     * @param weather
+     */
     public void save(Weather weather) {
         weatherRepository.save(weather);
     }
 
-    public List<Weather> findAll(Pageable pageable) {
-        return weatherRepository.findAll(pageable).getContent();
+    /**
+     * 삭제 처리
+     * @param weather
+     */
+    public void weatherDelete(Weather weather) {
+        weatherRepository.delete(weather);
     }
 
-    public Optional<Weather> findById(Long id){
-        Optional<Weather> result = weatherRepository.findById(id);
-        return result;
+    /**
+     * 마지막 데이터 조회
+     * @return
+     */
+    public Weather weatherLast() {
+        return weatherRepository.weatherLast();
     }
 
-    public void saveAuto() throws IOException, ParseException {
+    /**
+     * API 등록
+     */
+    public void saveAuto() {
 
         String base_date = DateUtil.getSimpleDateUtil("yyyyMMdd");
         String base_time = getWeatherTimeFormat(DateUtil.getSimpleDateUtil("HHmm"));
@@ -52,16 +91,34 @@ public class WeatherService {
         String ny = "107";
 
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?pageNo=1&numOfRows=12&dataType=JSON&ftype=SHRT"); /*URL*/
-        urlBuilder.append("&" + URLEncoder.encode("serviceKey","UTF-8") + "=" + URLEncoder.encode(apiKey, "UTF-8")); /*공공데이터포털에서 받은 인증키*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(base_date, "UTF-8")); /*15년 12월 1일 발표*/
-        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(base_time, "UTF-8")); /*06시 발표(정시단위)*/
-        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); /*예보지점의 X 좌표값*/
-        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); /*예보지점 Y 좌표*/
-        URL url = new URL(urlBuilder.toString());
+        URL url = null;
+        try {
+            urlBuilder.append("&" + URLEncoder.encode("serviceKey","UTF-8") + "=" + URLEncoder.encode(API_KEY, "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+            urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(base_date, "UTF-8")); /*15년 12월 1일 발표*/
+            urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(base_time, "UTF-8")); /*06시 발표(정시단위)*/
+            urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); /*예보지점의 X 좌표값*/
+            urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); /*예보지점 Y 좌표*/
+        } catch (UnsupportedEncodingException e) {
+            log.info("ERROR : " + e);
+        }
 
-        InputStreamReader isr = new InputStreamReader(url.openConnection().getInputStream(), "UTF-8");
+        try {
+            url = new URL(urlBuilder.toString());
+        } catch (MalformedURLException e) {
+            log.info("ERROR : " + e);
+        }
+
+        InputStreamReader isr = null;
         JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(isr);
+        JSONObject json = null;
+
+        try {
+            isr = new InputStreamReader(url.openConnection().getInputStream(), "UTF-8");
+            json = (JSONObject) parser.parse(isr);
+        } catch (IOException | ParseException e) {
+            log.info("ERROR : " + e);
+        }
+
         JSONObject response = (JSONObject) json.get("response");
         JSONObject body = (JSONObject) response.get("body");
         JSONObject items = (JSONObject) body.get("items");
@@ -118,13 +175,5 @@ public class WeatherService {
         else if(Integer.parseInt(time) > 2000 && Integer.parseInt(time) < 2200) return "1900";
         else if(Integer.parseInt(time) > 2200 && Integer.parseInt(time) < 2400) return "2100";
         else return "0000";
-    }
-
-    public Weather selectWeatherLast() {
-        return weatherRepository.selectWeatherLast();
-    }
-
-    public void weatherDelete(Weather weather) {
-        weatherRepository.delete(weather);
     }
 }
